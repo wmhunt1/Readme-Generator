87ca15e3a616d954412458901df0d8ed385aece1
Matt Hunt <wmatthunt@gmail.com>
2020-08-23 15:51:30 -0400

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,61 @@
+var fs = require("fs");
+var inquirer = require('inquirer');
 // array of questions for user
-const questions = [
+// const questions = [
 
-];
+// ];
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+// // function to write README file
+// function writeToFile(fileName, data) {
+// }
 
-// function to initialize program
-function init() {
+// // function to initialize program
+// function init() {
 
-}
+// }
 
-// function call to initialize program
-init();
+// // function call to initialize program
+// init();
+inquirer
+    .prompt([
+        /* Pass your questions in here */
+        {
+            name: 'title',
+            message: 'What is your project title?',
+            default: "Project 1"
+        },
+        {
+            name: 'description',
+            message: 'Describe your project',
+            default: "Details of Project"
+        },
+        {
+            name: 'install',
+            message: 'What is needed to install your project?',
+            default: "Just use npm install"
+        },
+        {
+            name: 'usage',
+            message: 'Provide instructions to use your project.',
+            default: "Just type Node index into the terminal"
+        },
+        {
+            name: 'credits',
+            message: 'List the collaborators and tutorials used.',
+            default: "None"
+        },
+    ])
+    .then(answers => {
+        console.info('Project Title:', answers.title);
+        console.info('Description:', answers.description);
+        console.info('Installation:', answers.install);
+        console.info('Usage:', answers.usage);
+        // Use user feedback for... whatever!!
+    })
+    .catch(error => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else when wrong
+        }
+    });
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Readme-Generator
 
-Description: A readme generator made using node js, inquirer and write.
+Description: A readme generator made using node js and inquirer. Github Link: https://github.com/wmhunt1/Readme-Generator
+
+Installation: Need to use npm install and npm install inquirer.
+
+Usage: 
+
+Credits: Tutorials used include: https://www.digitalocean.com/community/tutorials/nodejs-interactive-command-line-prompts, https://www.npmjs.com/package/inquirer
